2bb11911297bb4920817927defcf3b338d609b7e
Pavlik7777 <Pasha20080620@gmail.com>
2025-11-24 19:34:48 +0200

    pc

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,30 +1,43 @@
 import style from '../Footer/Footer.module.scss'
 import photo from '../assets/Storefront.png'
 import { Link } from 'react-router-dom'
-
+import photoo from  '../assets/User.png'
 function Footer() {
   return (
     <>
      <section className={style.Konech}  >
         <div className={style.levo}> 
             <Link to = {'/'}>
-                <ul className= {style.Konech}>
+                <div className= {style.Konechi}>
                 <img src={photo} alt="" />
                 <h1>NFT Marketplace</h1>
-                </ul>
+                </div>
             </Link>
             <p>NFT marketplace UI created with Anima for Figma.</p>
             <p>Join our community</p>
-            <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
+            <div className={style.photo} >
+                <img src= {photoo} alt="" />
+               <img src= {photoo} alt="" />
+                <img src= {photoo} alt="" />
+                <img src= {photoo} alt="" />
             </div>
         </div>
 
-        <div>2</div>
-        <div>3</div>
+        <div className={style.center} >
+          <h2>Explore</h2>
+          <p>Marketplace</p>
+          <p>Rankings</p>
+          <p>Connect a wallet</p>
+        </div>
+        
+        <div className={style.pravo}>
+          <h1> Join Our Weekly Digest</h1>
+          <p>Get explusive promotions & updates straight to your inbox.</p>
+            <div className= {style.inbut}>
+            <input type="text" placeholder='Usernam' />
+            <button>  Sub</button>
+            </div>
+        </div>
      </section>
     </>
   )
